089ef9526512adbee8f0367151935f2a82a8eab1
Gabriel Santana <gabriel_santana17@hotmail.com>
2021-10-01 16:07:58 -0300

    Add files via upload

---
+++ objetos.js
@@ -0,0 +1,11 @@
+let onibus = {
+   rodas: 8,
+   portas: 2,
+   passageiros: 40
+}
+
+console.log(onibus);
+
+console.log(onibus.rodas);
+console.log(onibus.portas);
+console.log(onibus.passageiros);
