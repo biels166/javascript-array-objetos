dd971ff8ae5ff7e38fa889cdbaca202a826c5d0a
Gabriel Santana <gabriel_santana17@hotmail.com>
2021-10-01 15:12:02 -0300

    Add files via upload

---
+++ lendo array.js
@@ -0,0 +1,10 @@
+let arr = [10,20,30,40,50];
+let arrb = [1,6];
+let arrc = [2,4,6,8,10];
+
+console.log(arr[0]);
+console.log(arr[2]);
+console.log(arr[3]);
+
+console.log(`O array [${arrb}] tem ${arrb.length} elementos`);
+console.log(`O array [${arrc}] tem ${arrc.length} elementos`);
