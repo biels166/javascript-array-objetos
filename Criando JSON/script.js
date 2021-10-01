985a0776581da7cf6b86bda935cff79674e4e386
Gabriel Santana <gabriel_santana17@hotmail.com>
2021-10-01 18:39:38 -0300

    update

---
+++ Criando JSON/script.js
@@ -0,0 +1,19 @@
+let pessoa = {
+    "name": "Gabriel",
+    "lastname": "Santos",
+    "age": 26,
+    "skills": "Full Stack",
+    "languages": ["Português", "Inglês", "Espanhol"]
+}
+
+console.log(pessoa.name);
+console.log(pessoa.lastname);
+console.log(pessoa.age);
+console.log(pessoa.skills);
+console.log(pessoa.languages);
+
+console.log(pessoa);
+
+
+
+
