8a3ab0ee7a62dd7ebbf93119a8b6c412c6fd5e6a
Gabriel Santana <gabriel_santana17@hotmail.com>
2021-10-01 18:20:08 -0300

    Update script.js

--- Iterando Elementos de Array/script.js
+++ Iterando Elementos de Array/script.js
@@ -1,5 +1,5 @@
 let arr = ["Gabriel", "Billy", "Lucas", "Renato", "Monica"];
 
-for(let i = 0; i <= 4; i++){
+for(let i = 0; i <= arr.length; i++){
     console.log(arr[i] + " " + arr[i].length);
 }
